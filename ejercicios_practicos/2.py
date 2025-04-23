59f8b933872e23f8494b5301c60e1e20e6216f4f
yasi6969 <santiagocn08@gmail.com>
2025-04-23 10:42:01 -0500

    ejercicio 2

---
+++ ejercicios_practicos/2.py
@@ -0,0 +1,26 @@
+#primero
+
+texto = input("intruduce un texto: ")
+tiempo_1_palabras = 0.5
+numero_de_palabras = len(texto.split(" "))
+
+print("------------")
+print(f"tardarias {numero_de_palabras * tiempo_1_palabras} segundos")
+print("------------")
+
+#sgundo 
+
+texto = input("intruduce un texto: ")
+tiempo_1_palabras = 0.5
+numero_de_palabras = len(texto.split(" "))
+tiempo_que_usa = numero_de_palabras * tiempo_1_palabras
+if tiempo_que_usa > 60:
+    print(f"para, estas escribiendo como que mucho, tardas {round(tiempo_que_usa, 2)} segundos")
+else:
+    print(f"tardas {round(tiempo_que_usa, 2)} segundos")
+
+#tercero
+
+tiempo_1_palabra_yo = tiempo_que_usa * 0.3
+print(f"yo tardaria {tiempo_1_palabra_yo} segundos")
+
