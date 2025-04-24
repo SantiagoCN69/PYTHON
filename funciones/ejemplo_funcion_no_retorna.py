aab9dde309d99de60934471512223e54bf8f5135
yasi6969 <santiagocn08@gmail.com>
2025-04-24 13:57:53 -0500

    contrasñea ejejmplo

--- funciones/ejemplo_funcion_no_retorna.py
+++ funciones/ejemplo_funcion_no_retorna.py
@@ -21,3 +21,29 @@ def crear_contraseña_random(num):
 
 # Llamamos la función con el argumento 1 como ejemplo
 crear_contraseña_random(1)
+
+
+#retornando el valor
+
+def crear_contraseña_random(num):
+    # Definimos una cadena de caracteres posibles para la contraseña
+    chars = "abcdefghij"
+    
+    # Convertimos el número recibido a string para poder indexar
+    num_entero = str(num)
+    # Tomamos el primer dígito del número recibido (en caso de que sea de más de una cifra)
+    num = int(num_entero[0])
+    
+    # Calculamos los índices para seleccionar caracteres
+    c1 = num - 2
+    c2 = num - 5
+    c3 = num + 5
+    
+    # Construimos la contraseña usando f-strings y los índices calculados
+    contraseña = f"{chars[c1]}{chars[c2]}{chars[c3]}{num*2}"
+    # Retornamos la contraseña generada
+    return contraseña
+
+# Llamamos la función con el argumento 1 como ejemplo
+password = crear_contraseña_random(1)
+print(f"tu contraseña es {password}")
