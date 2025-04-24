30e615b6f187ae3b2d451c8e5c2dd13a882617bf
yasi6969 <santiagocn08@gmail.com>
2025-04-24 09:30:43 -0500

    tuplas comentarios

--- bucles/for_tuplas.py
+++ bucles/for_tuplas.py
@@ -1,5 +1,7 @@
 # Ejemplo 5: Bucle for con tuplas
-pares = [(1, "uno"), (2, "dos"), (3, "tres")]
+pares = [(1, "uno", ), (2, "dos"), (3, "tres")]
 for numero, nombre in pares:
+    #numero y nombre son variables que almacenan el valor de cada elemento
+    #primer es el número, segundo es el nombre
     print(f"El número {numero} se escribe {nombre}")
 
